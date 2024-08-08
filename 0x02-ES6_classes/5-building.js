f43f82efa9ac04d9d020af53faaa02998c72dd8c
hiba <hebaaltayeb2@icloud.com>
2024-08-08 10:29:45 +0000

    fix 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,18 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class');
-    }
     this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    console.log(this._sqft);
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
