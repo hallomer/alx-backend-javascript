ff703eb51f7e2ca015c82e8823cb7263c1e463ec
hiba <hebaaltayeb2@icloud.com>
2024-10-04 20:07:18 +0300

    trying to fix 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write('This important software is now closing\n');
 });
